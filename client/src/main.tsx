7475c2a4dfa7eb072f787d08f3fe9579c75e2102
bobby854854854 <willybobby854@gmail.com>
2026-01-27 01:50:12 -0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,14 @@
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';  // This is the correct import for CSS
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
 
 // File: client/src/App.tsx
 import { Route, Switch } from 'wouter'
